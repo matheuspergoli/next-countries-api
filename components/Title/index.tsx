f5bb7200218a4dadbc232ed8886c1f68804af7ce
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-06 09:40:10 -0300

    Update

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Title() {
 	return (
 		<header className='p-3 border-b shadow-sm'>
-			<h1 className='text-2xl indent-5 font-bold'>Where in the world?</h1>
+			<h1 className='text-2xl indent-2 font-bold'>Where in the world?</h1>
 		</header>
 	)
 }
