3f45e2f79c3bec80c694c71f0fef80cb97053100
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-06 06:46:45 -0300

    Finalizando busca de países

--- components/Countries/index.tsx
+++ components/Countries/index.tsx
@@ -15,13 +15,35 @@ interface CountryData {
 
 function Countries({ data }) {
 	const { region } = React.useContext(CountryContext)
+	const [searchedCountry, setSearchedCountry] = React.useState([])
+
+	React.useEffect(() => {
+		async function fetchCountry() {
+			if (region.length > 3) {
+				const response = await fetch(
+					`https://restcountries.com/v3.1/name/${region}`
+				)
+				const data = await response.json()
+				setSearchedCountry(data)
+			}
+		}
+		fetchCountry()
+	}, [region])
 
 	return (
-		<main className='flex flex-wrap justify-evenly items-center gap-10 p-5'>
-			{data.map((country: CountryData, index: number) => (
-				<Country key={index} country={country} />
-			))}
-		</main>
+		<>
+			<section className='flex items-center justify-center'>
+				{searchedCountry &&
+					searchedCountry.map((country: CountryData, index: number) => (
+						<Country key={index} country={country} />
+					))}
+			</section>
+			<main className='flex flex-wrap justify-evenly items-center gap-10 p-5'>
+				{data.map((country: CountryData, index: number) => (
+					<Country key={index} country={country} />
+				))}
+			</main>
+		</>
 	)
 }
 
