2ea7258b4ab85de7bfdd3b11c97d911c54cc113f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-07 14:57:32 -0300

    Consumindo contexto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import { CountryContextProvider } from '../context/CountryContext'
+import { RegionContextProvider } from '../context/RegionContext'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<CountryContextProvider>
-			<Component {...pageProps} />
+			<RegionContextProvider>
+				<Component {...pageProps} />
+			</RegionContextProvider>
 		</CountryContextProvider>
 	)
 }
